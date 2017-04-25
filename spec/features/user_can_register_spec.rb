1b097ce3b6258c36ece7ffd616af1e0097b21ac8
Chad Ellison <chad.ellison0123@gmail.com>
2017-04-24 20:29:30 -0600

    update rails helper

--- spec/features/user_can_register_spec.rb
+++ spec/features/user_can_register_spec.rb
@@ -6,15 +6,19 @@ RSpec.feature 'user can register' do
       visit new_user_registration_path
 
       expect(page).to have_content('Sign up')
-      fill_in 'Email', with: 'jones@gmail.com'
-      fill_in 'First name', with: 'Bob'
-      fill_in 'Last name', with: 'Jones'
-      fill_in 'Password', with: 'password'
-      fill_in 'Password confirmation', with: 'password'
-      click_button 'Sign up'
+
+      expect do
+        fill_in 'Email', with: 'jones@gmail.com'
+        fill_in 'First name', with: 'Bob'
+        fill_in 'Last name', with: 'Jones'
+        fill_in 'Password', with: 'password'
+        fill_in 'Password confirmation', with: 'password'
+        click_button 'Sign up'
+      end.to change { User.count }.by(1)
 
       expect(current_path).to eq root_path
-      expect(page).to have_content 'Welcome! You have signed up successfully. Login to get started.'
+      message = 'Welcome! You have signed up successfully. Login to get started.'
+      expect(page).to have_content(message)
     end
   end
 
