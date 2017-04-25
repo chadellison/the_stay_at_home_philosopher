e414d1a52e8074d08a104320c4b805df858ae8dd
Chad Ellison <chad.ellison0123@gmail.com>
2017-04-24 19:43:59 -0600

    Authenticate user update specs

---
+++ spec/features/user_can_login_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.feature 'user can login' do
+  context 'with successful login' do
+    scenario 'user sees successful flash message' do
+      User.create(first_name: Faker::Name.first_name,
+                  last_name:  Faker::Name.last_name,
+                  email:      'jones@gmail.com',
+                  password:   'password')
+
+      visit new_user_session_path
+
+      expect(page).to have_content('Log in')
+      fill_in 'Email', with: 'jones@gmail.com'
+      fill_in 'Password', with: 'password'
+      click_button 'Log in'
+
+      expect(current_path).to eq root_path
+      expect(page).to have_content 'Signed in successfully'
+    end
+  end
+
+  context 'with unsuccessful log in' do
+    scenario 'user sees successful flash message' do
+      visit new_user_session_path
+
+      expect(page).to have_content('Log in')
+      fill_in 'Email', with: 'jones@gmail.com'
+      fill_in 'Password', with: 'password'
+      click_button 'Log in'
+
+      expect(current_path).to eq new_user_session_path
+      expect(page).to have_content 'Invalid Email or password.'
+    end
+  end
+end
